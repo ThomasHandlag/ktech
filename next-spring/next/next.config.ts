4ea039dd74a7105770a66b70956bfd8ca026509e
unknown <truongthuongvan203@gmail.com>
2025-07-17 11:26:24 +0700

    fix: unauthorized with vercel

--- next-spring/next/next.config.ts
+++ next-spring/next/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
         NEXTAUTH_SECRET: '4b5jMM1qXd+1/gv2eZ320qltFCrTGVG/qtsLbKMG+Go=',
-        NEXTAUTH_URL: 'http://localhost:3000'
     },
   // allow any remote pattern for images
   images: {
